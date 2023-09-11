51aa327222ce35d2180d5137b9cf88091b568650
chaehaeun <chae_3210@naver.com>
2023-09-11 18:49:08 +0900

    [🔧] 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import App from '@/App';
 import '@/index.css';
-import React from 'react';
+import router from '@/router';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
---
+++ src/router/index.tsx
@@ -0,0 +1,13 @@
+import App from '@/App';
+import { NotFound } from '@/pages';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+]);
+
+export default router;
