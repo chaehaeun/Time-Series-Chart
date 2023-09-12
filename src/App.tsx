95ae795c37710ea89465664bb49bce001a5d4e17
chaehaeun <chae_3210@naver.com>
2023-09-12 13:17:04 +0900

    [🎨 ] 로딩 스피너 추가 및 구역 리스트 코드 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { fetchingChartData } from '@/context';
 import { useState } from 'react';
 
 function App() {
-  const { chartData } = fetchingChartData();
+  const { chartData, LocationList, isLoading } = fetchingChartData();
   const [active, setActive] = useState<string | null>(null);
 
   const handleButtonClick = (id: string | null) => {
@@ -13,28 +13,46 @@ function App() {
   return (
     <>
       <Header />
-      <main className="mx-auto w-[1280px] my-5">
-        <ul className="flex gap-2 py-2">
-          <li key={'entire'}>
-            <Button
-              isActive={active === null ? true : false}
-              onClick={() => {
-                setActive(null);
-              }}
-            >
-              전체
-            </Button>
-          </li>
-          {Array.from(new Set(chartData.map((item) => item.id))).map((id, idx) => (
-            <li key={idx}>
-              <Button isActive={active === id ? true : false} onClick={() => handleButtonClick(id)}>
-                {id}
+      {isLoading ? (
+        <div className="flex items-center justify-center mt-60">
+          <svg
+            className="w-32 h-32 mr-3 -ml-1 text-white animate-spin"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      ) : (
+        <main className="mx-auto w-[1280px] my-5">
+          <ul className="flex gap-2 py-2">
+            <li key={'entire'}>
+              <Button
+                isActive={active === null ? true : false}
+                onClick={() => {
+                  setActive(null);
+                }}
+              >
+                전체
               </Button>
             </li>
-          ))}
-        </ul>
-        {chartData.length !== 0 && <Chart active={active} setActive={setActive} />}
-      </main>
+            {LocationList.map((id, idx) => (
+              <li key={idx}>
+                <Button isActive={active === id ? true : false} onClick={() => handleButtonClick(id)}>
+                  {id}
+                </Button>
+              </li>
+            ))}
+          </ul>
+          {chartData.length !== 0 && <Chart active={active} setActive={setActive} />}
+        </main>
+      )}
     </>
   );
 }
