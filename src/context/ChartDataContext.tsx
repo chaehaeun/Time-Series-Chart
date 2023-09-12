f1ca9aa00c489c2f80b73b58a1d72788bea15b43
chaehaeun <chae_3210@naver.com>
2023-09-12 13:15:50 +0900

    [✨] 로딩 추가

--- src/context/ChartDataContext.tsx
+++ src/context/ChartDataContext.tsx
@@ -5,33 +5,45 @@ import { createContext, useContext, useEffect, useState } from 'react';
 interface ChartDataContextType {
   chartData: ChartDataType[];
   LocationList: string[];
+  isLoading: boolean;
 }
 
 const ChartDataContext = createContext<ChartDataContextType>({
   chartData: [],
   LocationList: [],
+  isLoading: false,
 });
 
 export const ChartDataProvider = ({ children }: any) => {
   const [chartData, setChartData] = useState<ChartDataType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   useEffect(() => {
     const fetchChartData = async () => {
-      const res: ChartDataType = await getData();
+      try {
+        setIsLoading(true);
+        const res: ChartDataType = await getData();
 
-      console.log(res);
-      const arrData = Object.entries(res).map(([time, item]) => ({
-        time: new Date(time).toLocaleTimeString(),
-        ...item,
-      }));
+        const arrData = Object.entries(res).map(([time, item]) => ({
+          time: new Date(time).toLocaleTimeString(),
+          ...item,
+        }));
 
-      setChartData(arrData);
+        setChartData(arrData);
+      } catch {
+        console.error('데이터 요청 실패');
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchChartData();
   }, []);
 
   const LocationList = Array.from(new Set(chartData.map((item) => item.id)));
 
-  return <ChartDataContext.Provider value={{ chartData, LocationList }}>{children}</ChartDataContext.Provider>;
+  return (
+    <ChartDataContext.Provider value={{ chartData, LocationList, isLoading }}>{children}</ChartDataContext.Provider>
+  );
 };
 
 export const fetchingChartData = () => useContext(ChartDataContext);
